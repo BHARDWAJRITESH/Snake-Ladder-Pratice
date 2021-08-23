57b01b26cdb3e715d6ee768666f23803f8556eb7
BHARDWAJRITESH <ritesh844581@gmail.com>
2021-08-23 19:05:17 +0530

    The Number of times the dice was played to win the game && position after every dice

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,7 +3,9 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome To Snake And Ladder Simulator");
         int position= 0;
+        int diceRollNo=0;
         while (position != 100) {
+        diceRollNo++;
         int diceRoll=1+(int)Math.floor(Math.random()*10)%6;
         System.out.println("Player Rolls:" +diceRoll);
         int checkOption=1+(int)Math.floor(Math.random()*10)%3;
@@ -12,6 +14,14 @@ public class SnakeAndLadder {
         }
         else if(checkOption==1){
             System.out.println("Ladder Comes");
+              if(position+diceRoll<=100)
+                {
+                    position+=diceRoll;
+                }
+                else if(position+diceRoll>100)
+                {
+                    continue;
+                }
            }
         else{
             System.out.println("Snake Comes");
@@ -21,6 +31,10 @@ public class SnakeAndLadder {
             position-=diceRoll;
         }
      }
-   }
- }
+        System.out.println(diceRollNo);
+        System.out.println("Player Position After Dice Roll "+ position);
+}
+
+        System.out.println("Number Of Dice Roll To Win " + diceRollNo);
+}
 }
