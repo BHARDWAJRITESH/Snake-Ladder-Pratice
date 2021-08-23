77602af93562dca16c76ddccc1732a8a51507008
BHARDWAJRITESH <ritesh844581@gmail.com>
2021-08-23 18:09:33 +0530

    The Player rolls the die

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome To Snake And Ladder Simulator");
@@ -8,7 +9,8 @@ public class SnakeAndLadder {
         System.out.println("Game Start");
         playerPos = start;
         System.out.println("Player at Position:" +playerPos);
-
+        int diceRoll=1+(int)Math.floor(Math.random()*10)%6;
+        System.out.println("Player Rolls:" +diceRoll);
 
 }
 }
