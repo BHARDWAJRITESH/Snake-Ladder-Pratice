6f2a38cebd544500eff705efb9e8771066b1b7af
BHARDWAJRITESH <ritesh844581@gmail.com>
2021-08-23 18:29:52 +0530

    The Player then checks for

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,15 +2,21 @@ import java.util.Random;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome To Snake And Ladder Simulator");
-        int start = 0;
-        int Finish = 100;
-        int playerPos;
-
-        System.out.println("Game Start");
-        playerPos = start;
-        System.out.println("Player at Position:" +playerPos);
+        int position= 0;
         int diceRoll=1+(int)Math.floor(Math.random()*10)%6;
         System.out.println("Player Rolls:" +diceRoll);
+        int checkOption=1+(int)Math.floor(Math.random()*10)%3;
+        if (checkOption==0) {
+            System.out.println("No Play");
+        }
+        else if(checkOption==1){
+            System.out.println("Ladder Comes");
+            position+=diceRoll;
+        }
+        else{
+            System.out.println("Snake Comes");
+            position-=diceRoll;
+        }
 
 }
 }
