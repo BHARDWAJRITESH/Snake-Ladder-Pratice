0ce6b0e9760eb9dff610f759e18d7f7c9a1fbf5f
BHARDWAJRITESH <ritesh844581@gmail.com>
2021-08-23 18:04:10 +0530

    Single player at start Position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,5 +1,14 @@
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome To Snake And Ladder Simulator");
+        int start = 0;
+        int Finish = 100;
+        int playerPos;
+
+        System.out.println("Game Start");
+        playerPos = start;
+        System.out.println("Player at Position:" +playerPos);
+
+
 }
 }
